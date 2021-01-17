064e340e0a1aa5a54c2f53f53f407725389d684c
John Harris <vosm803@gmail.com>
2021-01-17 14:45:20 -0500

    Removed commented code

--- src/main/java/com/github/harrisj09/irc/client/Main.java
+++ src/main/java/com/github/harrisj09/irc/client/Main.java
@@ -60,53 +60,13 @@ public class Main extends Application {
             // change height and width here
         };
         startView.getButton().addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
-        /*
-                EventHandler<MouseEvent> eventHandler = e -> {
-            Scene scene = null;
-            try {
-                scene = canConnect(ip.getText(), port.getText(), userName.getText(), primaryStage);
-            } catch (URISyntaxException | IOException | InterruptedException uriSyntaxException) {
-                uriSyntaxException.printStackTrace();
-            }
-            if (scene != null) {
-                primaryStage.setScene(scene);
-                primaryStage.show();
-                primaryStage.setMaximized(true);
-            }
-        };
-        button.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
-         */
-        /*
-        This works
-        TextField ip = new TextField();
-        TextField port = new TextField();
-        TextField userName = new TextField();
-        HBox ipBox = new HBox(new Text("IP"), ip);
-        HBox portBox = new HBox(new Text("Port"), port);
-        HBox userNameBox = new HBox(new Text("UserName"), userName);
-        Button button = new Button("Submit");
-        EventHandler<MouseEvent> eventHandler = e -> {
-            Scene scene = null;
-            try {
-                scene = canConnect(ip.getText(), port.getText(), userName.getText(), primaryStage);
-            } catch (URISyntaxException | IOException | InterruptedException uriSyntaxException) {
-                uriSyntaxException.printStackTrace();
-            }
-            if (scene != null) {
-                primaryStage.setScene(scene);
-                primaryStage.show();
-                primaryStage.setMaximized(true);
-            }
-        };
-        button.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
-        Scene scene = new Scene(new VBox(ipBox, portBox, userNameBox, button), 400, 400);
-        *//*primaryStage.setScene(scene);*/
         primaryStage.show();
     }
 
     public String canConnect(String ip, String port, String username) throws URISyntaxException, IOException, InterruptedException {
         HttpClient client = HttpClient.newBuilder().build();
-        HttpRequest build = HttpRequest.newBuilder().GET().uri(new URI("http://" + username + ":" + port + "/connect/" + username)).build();
+        HttpRequest build = HttpRequest.newBuilder().GET().uri(
+                new URI("http://" + username + ":" + port + "/connect/" + username)).build();
         HttpResponse<String> send = HttpClient.newBuilder()
                 .build()
                 .send(build, HttpResponse.BodyHandlers.ofString());
